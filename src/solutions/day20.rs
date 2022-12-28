8764b202730ce89b022d2b8e4f28567f41538199
Anton Älgmyr <anton@algmyr.se>
2022-12-28 03:19:44 +0100

    Day 20 sqrt decomp

--- src/solutions/day20.rs
+++ src/solutions/day20.rs
@@ -6,37 +6,111 @@ pub fn parse_input(fname: &str) -> AocResult<Vec<i64>> {
   crate::utils::read_all_signed_nums(fname)
 }
 
-struct C<'a> {
-  indices: Vec<usize>,
-  shifts: &'a [i64],
+struct Block {
+  data: Vec<usize>,
+  contains: Vec<bool>,
 }
 
-impl<'a> C<'a> {
-  fn new(shifts: &'a [i64]) -> Self {
-    C { indices: (0..shifts.len()).collect_vec(), shifts }
+impl Block {
+  fn new(chunk: &[usize], numel: usize) -> Self {
+    let data = chunk.to_vec();
+    let mut contains = vec![false; numel];
+    for x in chunk {
+      contains[*x] = true;
+    }
+    Self { data, contains }
+  }
+}
+
+struct Blocks {
+  blocks: Vec<Block>,
+}
+
+impl Blocks {
+  fn new(n: usize) -> Self {
+    let mut blocks = vec![];
+    for chunk in (0..n).chunks(100).into_iter() {
+      let block = Block::new(&chunk.collect_vec(), n);
+      blocks.push(block);
+    }
+    Self { blocks }
   }
 
+  fn find(&self, element: usize) -> usize {
+    let mut base = 0;
+    for block in &self.blocks {
+      if block.contains[element] {
+        let ix = block.data.iter().position(|&x| x == element).expect(":<");
+        return base + ix;
+      }
+      base += block.data.len();
+    }
+    unreachable!("Element not found.");
+  }
+
+  fn find_and_erase(&mut self, element: usize) -> usize {
+    let mut base = 0;
+    for block in &mut self.blocks {
+      if block.contains[element] {
+        let ix = block.data.iter().position(|&x| x == element).expect(":<");
+        block.data.remove(ix);
+        block.contains[element] = false;
+        return base + ix;
+      }
+      base += block.data.len();
+    }
+    unreachable!("Element not found.");
+  }
+
+  fn get(&self, mut index: usize) -> usize {
+    for block in &self.blocks {
+      if index <= block.data.len() {
+        return block.data[index];
+      }
+      index -= block.data.len();
+    }
+    unreachable!("Element at index not found.");
+  }
+
+  fn insert(&mut self, mut index: usize, element: usize) {
+    for block in &mut self.blocks {
+      if index <= block.data.len() {
+        block.data.insert(index, element);
+        block.contains[element] = true;
+        break;
+      }
+      index -= block.data.len();
+    }
+  }
+}
+
+struct C<'a> {
+  blocks: Blocks,
+  shifts: &'a [i64],
+}
+impl<'a> C<'a> {
+  fn new(shifts: &'a [i64]) -> Self { C { blocks: Blocks::new(shifts.len()), shifts } }
+
   fn shift(&mut self, i: usize, shift: i64) {
-    let n = self.indices.len();
-    let ix = self.indices.iter().position(|&x| x == i).expect(":<");
-    self.indices.remove(ix);
+    let ix = self.blocks.find_and_erase(i);
+    let n = self.shifts.len();
     let target = (ix as i64 + shift - 1).rem_euclid(n as i64 - 1) + 1;
-    self.indices.insert(target as usize, i);
+    self.blocks.insert(target as usize, i);
   }
 
   fn shuffle(&mut self) {
-    for i in 0..self.indices.len() {
+    for i in 0..self.shifts.len() {
       self.shift(i, self.shifts[i]);
     }
   }
 
   fn find_zero(&self) -> usize {
     let ix = self.shifts.iter().position(|&x| x == 0).expect(":<");
-    self.indices.iter().position(|&i| i == ix).expect(":<")
+    self.blocks.find(ix)
   }
 
   fn get_cyclic(&self, ix: usize) -> i64 {
-    self.shifts[self.indices[ix % self.indices.len()]]
+    self.shifts[self.blocks.get(ix % self.shifts.len())]
   }
 }
 
